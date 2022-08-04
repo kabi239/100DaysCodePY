1dba3bc73e2083742dff132f8e81589f6e0be317
kabi239 <kavyasharma239@gmail.com>
2022-08-04 23:18:56 +0530

    DAY 1

---
+++ DAY1.py
@@ -0,0 +1,5 @@
+# Band Name Genrator
+print("Welcome to Band Name Genrator")
+a=input("What's name of the city you grew up in ?\n")
+b=input("Whats your pet's name ?\n")
+print("Your band name coud be "+a+" "+b)
